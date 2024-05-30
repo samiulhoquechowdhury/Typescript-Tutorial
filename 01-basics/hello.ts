6a0a4afd3e8421ea57d40a7344dadf8f262e4449
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-05-30 17:18:06 +0530

    Added comments

--- 01-basics/hello.ts
+++ 01-basics/hello.ts
@@ -2,4 +2,5 @@ console.log("Hello, World!");
 /**
  * Typescript is javascript with added syntax for types.
  * TypeScript is a syntactic superset of javascript wich adds static types.
+ * Typescript is all about type safety.
  */
