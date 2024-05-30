1399669398f8f80c79a081901618bd0cc60719ab
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-05-30 17:36:29 +0530

    Added more notes

--- 01-basics/hello.ts
+++ 01-basics/hello.ts
@@ -3,4 +3,10 @@ console.log("Hello, World!");
  * Typescript is javascript with added syntax for types.
  * TypeScript is a syntactic superset of javascript wich adds static types.
  * Typescript is all about type safety.
+ * 
+ * 
+ * 
+ * what typescript does:
+ *              static checking ==> Anayze the code as we type.thats it
+ * Typescript codes is still run in jsvascript so we can say that ts is a development tool.
  */
